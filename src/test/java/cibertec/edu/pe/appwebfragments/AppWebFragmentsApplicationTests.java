28a5c46d9f02765fb5ea6314fb6d549891ffc2aa
Chiszopo <chiszopo@gmail.com>
2023-10-01 09:44:06 -0500

    primer commit

---
+++ src/test/java/cibertec/edu/pe/appwebfragments/AppWebFragmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package cibertec.edu.pe.appwebfragments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppWebFragmentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
